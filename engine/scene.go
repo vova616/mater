4ccd1df312645f0285f7e875e4bbd5ff9beed73e
Matteo Goggi <matteo.goggi@gmail.com>
2012-01-20 13:20:59 +0100

    added missing package mater/engine

---
+++ engine/scene.go
@@ -0,0 +1,46 @@
+package engine
+
+import (
+	"github.com/teomat/mater/collision"
+)
+
+type Callbacks struct {
+	OnNewComponent func(entity *Entity, comp Component)
+}
+
+type Scene struct {
+	Space        *collision.Space
+	StaticEntity Entity
+	Entities     map[int]*Entity
+	Callbacks    Callbacks
+}
+
+func (scene *Scene) Init() {
+	scene.Space = collision.NewSpace()
+	scene.StaticEntity.Init()
+	scene.Entities = make(map[int]*Entity, 32)
+}
+
+func (scene *Scene) Update(dt float64) {
+	scene.Space.Step(dt)
+	scene.StaticEntity.Update(dt)
+	for _, entity := range scene.Entities {
+		if entity.Enabled {
+			entity.Update(dt)
+		}
+	}
+}
+
+func (scene *Scene) AddEntity(entity *Entity) {
+	scene.Entities[entity.Id()] = entity
+}
+
+func (scene *Scene) RemoveEntity(entity *Entity) {
+	delete(scene.Entities, entity.Id())
+}
+
+func (scene *Scene) Destroy() {
+	for _, entity := range scene.Entities {
+		entity.Destroy()
+	}
+}
