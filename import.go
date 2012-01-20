c23c73056ca4f0037f92f62517598ca5fffeab30
Matteo Goggi <matteo.goggi@gmail.com>
2012-01-20 11:41:04 +0100

    goinstall github.com/teomat/mater now installs all mater packages

---
+++ import.go
@@ -0,0 +1,13 @@
+package mater
+
+import (
+	_ "github.com/teomat/mater/aabb"
+	_ "github.com/teomat/mater/camera"
+	_ "github.com/teomat/mater/collision"
+	_ "github.com/teomat/mater/components"
+	_ "github.com/teomat/mater/engine"
+	_ "github.com/teomat/mater/render"
+	_ "github.com/teomat/mater/texutil"
+	_ "github.com/teomat/mater/transform"
+	_ "github.com/teomat/mater/vect"
+)
