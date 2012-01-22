a3b9cdc90c61963d87e2dcf0034b92fa5d07d784
Matteo Goggi <matteo.goggi@gmail.com>
2012-01-22 10:24:43 +0100

    updated createExamples to also create the files in saves/tests/

--- examples/main/createExamples.go
+++ examples/main/createExamples.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/teomat/mater/collision"
 	"github.com/teomat/mater/vect"
+	"github.com/teomat/mater/engine"
+	"github.com/teomat/mater/camera"
 	"os"
 )
 
@@ -36,6 +38,8 @@ func saveToFile(i interface{}, fileName string) {
 func allExamples() {
 	examplesPath = "examples/"
 	collisionExamples()
+	examplesPath = "tests/"
+	collisionTests()
 }
 
 //Creates examples for mater/collision.
@@ -81,5 +85,116 @@ func collisionExamples() {
 		space := collision.NewSpace()
 
 		saveToFile(space, "collision/space")
+	}
+}
+
+func collisionTests() {
+	scene := new(engine.Scene)
+	scene.Init()
+
+	{
+		cam := new(camera.Camera)
+		cam.Scale = vect.Vect{32, 32}
+		cam.IsMainCamera = true
+
+		scene.StaticEntity.AddComponent(cam)
+	}
+
+	//circle-segment collision
+	{
+		space := collision.NewSpace()
+		scene.Space = space
+		space.Gravity = vect.Vect{0, 10}
+
+		{
+			body := collision.NewBody(collision.BodyType_Static)
+			body.Transform.Position = vect.Vect{-3, 0}
+			body.Transform.SetAngle(0.5)
+
+			seg := collision.NewSegment(vect.Vect{5, 0}, vect.Vect{-5, 0}, 0.0)
+			body.AddShape(seg)
+
+			space.AddBody(body)
+		}
+
+		{
+			body := collision.NewBody(collision.BodyType_Static)
+			body.Transform.Position = vect.Vect{3, 0}
+			body.Transform.SetAngle(-0.5)
+
+			seg := collision.NewSegment(vect.Vect{5, 0}, vect.Vect{-5, 0}, 1.0)
+			body.AddShape(seg)
+
+			space.AddBody(body)
+		}
+
+		{
+			body := collision.NewBody(collision.BodyType_Dynamic)
+			body.Transform.Position = vect.Vect{6, -7}
+			body.Transform.SetAngle(0)
+
+			circle := collision.NewCircle(vect.Vect{0, 0}, 1.0)
+			body.AddShape(circle)
+
+			space.AddBody(body)
+		}
+
+		saveToFile(scene, "circle-segment")
+	}
+
+	//circle-polygon collision
+	{
+		space := collision.NewSpace()
+		scene.Space = space
+		space.Gravity = vect.Vect{0, 10}
+
+		{
+			body := collision.NewBody(collision.BodyType_Static)
+			body.Transform.Position = vect.Vect{-3, 0}
+			body.Transform.SetAngle(0.5)
+
+			verts := collision.Vertices{
+				{-5, -1},
+				{-5, 1},
+				{5, 1},
+				{5, -1},
+			}
+
+			poly := collision.NewPolygon(verts, vect.Vect{})
+			body.AddShape(poly)
+
+			space.AddBody(body)
+		}
+
+		{
+			body := collision.NewBody(collision.BodyType_Static)
+			body.Transform.Position = vect.Vect{3, 0}
+			body.Transform.SetAngle(-0.5)
+
+			verts := collision.Vertices{
+				{-5, -1},
+				{-5, 1},
+				{5, 1},
+				{5, -1},
+			}
+
+			poly := collision.NewPolygon(verts, vect.Vect{})
+			body.AddShape(poly)
+
+			space.AddBody(body)
+		}
+
+		{
+			body := collision.NewBody(collision.BodyType_Dynamic)
+			body.Transform.Position = vect.Vect{6, -7}
+			body.Transform.SetAngle(0)
+
+			circle := collision.NewCircle(vect.Vect{0, 0}, 1.0)
+			body.AddShape(circle)
+
+			space.AddBody(body)
+		}
+		
+		saveToFile(scene, "circle-polygon")
 	}
 }
